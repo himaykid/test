ac83f03d122e926636d99045a49eb2d59b438e72
himaykid <happymaykid@sina.com>
2016-12-23 14:36:12 +0800

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<test.h>
+using namespace std;
+int main()
+{
+  cout<<"fa"<<std::endl;
+  int cc;
+}
+int b()
+{
+  int abc;
+  int  d;
+}
+using namespace st;
+#include <vector>
